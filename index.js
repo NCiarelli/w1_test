8977565cdcac1795436d237b67259a73036ad2f1
Nicholas Ciarelli <nicholas.ciarelli@leverx.com>
2020-03-27 12:39:38 -0400

    Enabled the code to actually get a value from the sensor and put the actual UID of the sensor in the code.

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 
 var W1Temp = require('w1temp');
  
-// turn on gpio pin 13 as W1 power if you want to
+// turn on gpio pin 25 as W1 power if you want to
 W1Temp.setGpioPower(25);
  
-// set gpio pin 6 to use as W1 data channel
+// set gpio pin 8 to use as W1 data channel
 // if is not set by instructions above (required root permissions)
 W1Temp.setGpioData(8);
  
@@ -16,16 +16,16 @@ W1Temp.getSensorsUids().then(function (sensorsUids) {
   console.log(sensorsUids);
 });
  
-// // get instance of temperature sensor
-// W1Temp.getSensor('28-00000636a3e3').then(function (sensor) {
+// get instance of temperature sensor
+W1Temp.getSensor('28-031683a0feff').then(function (sensor) {
  
-//   // print actual temperature
-//   var temp = sensor.getTemperature();
-//   console.log('Actual temp:', temp, '°C');
+  // print actual temperature
+  var temp = sensor.getTemperature();
+  console.log('Actual temp:', temp, '°C');
  
-//   // print actual temperature on changed
-//   sensor.on('change', function (temp) {
-//     console.log('Temp changed:', temp, '°C');
-//   });
+  // print actual temperature on changed
+  sensor.on('change', function (temp) {
+    console.log('Temp changed:', temp, '°C');
+  });
  
-// });
+});
